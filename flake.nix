1801d20b68106f4936427edd11a6c0eab325f39e
seventhse <season.sevent@icloud.com>
2025-04-13 01:32:20 +0800

    Add node env config

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Full developer env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            direnv
+            nix-direnv
+            git
+            nodejs_22
+            pnpm_10
+          ];
+
+          shellHook = ''
+            echo "🔧 Development environment ready!"
+          '';
+        };
+      });
+}
